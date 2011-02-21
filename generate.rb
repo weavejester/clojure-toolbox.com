92d086684507cfdfa3f00368e2b88f267608ea75
James Reeves <jreeves@weavejester.com>
2011-02-21 21:19:32 +0000

    Multiple categories allowed

--- generate.rb
+++ generate.rb
@@ -8,7 +8,9 @@ projects = YAML.load_file("projects.yml").values.sort_by { |p| p['name'] }
 @categories = Hash.new { |h, k| h[k] = [] }
 
 projects.each do |project|
-  @categories[project['category']] << project
+  project['category'].split(',').each do |category|
+    @categories[category.strip] << project
+  end
 end
 
 template = ERB.new(File.read("src/index.html.erb"))
