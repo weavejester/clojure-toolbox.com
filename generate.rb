38c9f65a55dea9aada3c9f51536e71193eae9967
James Reeves <jreeves@weavejester.com>
2011-02-21 02:25:06 +0000

    Generate page from yaml file

---
+++ generate.rb
@@ -0,0 +1,18 @@
+#!/usr/bin/env ruby
+require 'yaml'
+require 'erb'
+require 'set'
+
+projects = YAML.load_file("projects.yml").values
+
+@categories = Hash.new { |h, k| h[k] = [] }
+
+projects.each do |project|
+  @categories[project['category']] << project
+end
+
+template = ERB.new(File.read("src/index.html.erb"))
+
+File.open("index.html", "w") do |f|
+  f.write(template.result)
+end
