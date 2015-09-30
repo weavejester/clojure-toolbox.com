055801684aa0d61d9bb18901c75f918e1ca81cf6
James Reeves <jreeves@weavejester.com>
2015-10-01 00:35:03 +0100

    Move custom JS into own file

---
+++ js/main.js
@@ -0,0 +1,16 @@
+$('#project-list').
+    masonry({ transitionDuration: 0, columnWidth: 260 }).
+    css({visibility: 'visible'});
+$('footer').css({visibility: 'visible'});
+
+$('#search-query').on('change keyup paste input', function(event) {
+    var query    = $.trim(event.target.value.toLowerCase());
+    var projects = $('#project-list li');
+    if (query == '') {
+        projects.show();
+    } else {
+        projects.hide().filter('[data-search*="' + query + '"]').show();
+    }
+    $('#project-list .category').show().not(':has(li:visible)').hide();
+    $('#project-list').masonry('layout');
+});
