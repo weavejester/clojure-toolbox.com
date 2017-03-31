68f3853fda4bc169d0baaf3e515bdc4f102ab521
James Reeves <jreeves@weavejester.com>
2017-04-01 00:42:25 +0100

    Add script for uploading certificate

---
+++ script/upload-cert.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+letsencrypt --agree-tos -a certbot-s3front:auth \
+--certbot-s3front:auth-s3-bucket www.clojure-toolbox.com \
+--certbot-s3front:auth-s3-region us-east-1 \
+-i certbot-s3front:installer \
+--certbot-s3front:installer-cf-distribution-id E3U0YV0QLJTML9 \
+-d www.clojure-toolbox.com
