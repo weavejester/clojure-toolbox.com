d3271a72b9c95e56a808e8be5f9848e34a71922b
James Reeves <jreeves@weavejester.com>
2018-09-27 18:21:42 +0100

    Rename letsencrypt command to certbot

--- script/upload-cert.sh
+++ script/upload-cert.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-letsencrypt --agree-tos -a certbot-s3front:auth \
+certbot --agree-tos -a certbot-s3front:auth \
 --certbot-s3front:auth-s3-bucket www.clojure-toolbox.com \
 --certbot-s3front:auth-s3-region us-east-1 \
 -i certbot-s3front:installer \
