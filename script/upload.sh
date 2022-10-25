17de40499e53cad519b909393b6bf23800b71c60
James Reeves <jreeves@weavejester.com>
2022-10-25 22:56:50 +0100

    Stop s3cmd using mime magic
    Fixes #456.

--- script/upload.sh
+++ script/upload.sh
@@ -1,2 +1,2 @@
-s3cmd put -P *.html *.txt *.png s3://www.clojure-toolbox.com/
-s3cmd put -Pr css img js s3://www.clojure-toolbox.com/
+s3cmd put -P --no-mime-magic *.html *.txt *.png s3://www.clojure-toolbox.com/
+s3cmd put -Pr --no-mime-magic css img js s3://www.clojure-toolbox.com/
